7c4359ab9a4b31ef232dedc7a0d99edd0367cb00
fady-hue <fady@torod.co>
2026-06-21 04:06:24 +0300

    temp: remove ClerkProvider to unblock deployment and diagnose Clerk key issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,16 +13,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en" dir="ltr">
-        <head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        </head>
-        <body className="bg-[#0A0A0F] text-[#F0F0F5] antialiased">
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" dir="ltr">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
+      <body className="bg-[#0A0A0F] text-[#F0F0F5] antialiased">
+        {children}
+      </body>
+    </html>
   );
 }
